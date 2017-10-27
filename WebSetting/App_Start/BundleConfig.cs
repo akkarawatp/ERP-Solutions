41928661ab693b6c0ac615ca8b7b070cd7b96b7d
Akkarawat Pootthajun <akkarawatp@gmail.com>
2017-10-28 01:08:25 +0700

    Update Menu Layout

--- WebSetting/App_Start/BundleConfig.cs
+++ WebSetting/App_Start/BundleConfig.cs
@@ -13,42 +13,31 @@ namespace WebSetting
             bundles.IgnoreList.Clear();
             AddDefaultIgnorePatterns(bundles.IgnoreList);
 
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.min.js",
-                        "~/Scripts/jquery.form.min.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.min.js",
+            //            "~/Scripts/jquery.form.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive.min.js",
-                        "~/Scripts/jquery.validate.min.js",
-                        "~/Scripts/jquery.validate.unobtrusive.min.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.unobtrusive.min.js",
+            //            "~/Scripts/jquery.validate.min.js",
+            //            "~/Scripts/jquery.validate.unobtrusive.min.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr.min.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr.min.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/Site.css"));
-            bundles.Add(new StyleBundle("~/Content/select2").Include(
-                        "~/Content/select2-bootstrap/select2.css",
-                        "~/Content/select2-bootstrap/select2-bootstrap.css"));
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //        "~/Content/Site.css",
+            //        "~/Content/bootstrap-responsive.min.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.min.js"));
-            bundles.Add(new ScriptBundle("~/bundles/app").Include("~/Scripts/app.js"));
-            bundles.Add(new StyleBundle("~/Content/bootstrap/base").Include("~/Content/bootstrap.min.css"));
-            bundles.Add(new StyleBundle("~/Content/bootstrap/theme").Include("~/Content/bootstrap-theme.min.css"));
-            bundles.Add(new StyleBundle("~/Content/font-awesome").Include("~/Content/font-awesome.min.css"));
+            //bundles.Add(new StyleBundle("~/Content/select2").Include(
+            //            "~/Content/select2-bootstrap/select2.css",
+            //            "~/Content/select2-bootstrap/select2-bootstrap.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/sr")
-                .Include("~/Scripts/bootstraptable/bootstrap-table.min.js")
-                .Include("~/Scripts/bootstraptable/bootstrap-table-th-TH.min.js")
-                .Include("~/Scripts/autoNumeric.min.js")
-                .Include("~/Scripts/sr.js")
-                );
-
-            bundles.Add(new StyleBundle("~/Content/sr")
-                .Include("~/Scripts/bootstraptable/bootstrap-table.min.css")
-                .Include("~/Content/sr.css")
-                );
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.min.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/app").Include("~/Scripts/app.js"));
+            //bundles.Add(new StyleBundle("~/Content/bootstrap/base").Include("~/Content/bootstrap.min.css", "~/Content/base/jquery-ui.css", "~/Content/base/jquery.ui.theme.css"));
+            //bundles.Add(new StyleBundle("~/Content/bootstrap/theme").Include("~/Content/bootstrap-theme.min.css"));
+            //bundles.Add(new StyleBundle("~/Content/font-awesome").Include("~/Content/font-awesome.min.css"));
         }
 
         public static void AddDefaultIgnorePatterns(IgnoreList ignoreList)
