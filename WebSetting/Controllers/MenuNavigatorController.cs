3560d895f4982856881c3ff7163483e322265caf
Akkarawat Pootthajun <akkarawatp@gmail.com>
2017-10-27 23:20:10 +0700

    Add Flat Template

---
+++ WebSetting/Controllers/MenuNavigatorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSetting.Controllers
+{
+    public class MenuNavigatorController : Controller
+    {
+        // GET: MenuNavigator
+        public ActionResult MainMenu()
+        {
+            return PartialView("~/Views/MenuNavigator/MainMenu.cshtml");
+        }
+    }
+}
