d7882b78ab1c2013d750e824b151bf3127fd8429
Akkarawat Pootthajun <akkarawatp@gmail.com>
2017-10-30 00:32:13 +0700

    ปรับ Style Sheet อีกที

--- WebSetting/App_Start/BundleConfig.cs
+++ WebSetting/App_Start/BundleConfig.cs
@@ -13,31 +13,34 @@ namespace WebSetting
             bundles.IgnoreList.Clear();
             AddDefaultIgnorePatterns(bundles.IgnoreList);
 
-            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-            //            "~/Scripts/jquery-{version}.min.js",
-            //            "~/Scripts/jquery.form.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/js/jquery-{version}.min.js",
+                        "~/js/jquery.form.min.js"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-            //            "~/Scripts/jquery.unobtrusive.min.js",
-            //            "~/Scripts/jquery.validate.min.js",
-            //            "~/Scripts/jquery.validate.unobtrusive.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/js/jquery.unobtrusive.min.js",
+                        "~/js/jquery.validate.min.js",
+                        "~/js/jquery.validate.unobtrusive.min.js"));
 
             //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
             //            "~/Scripts/modernizr.min.js"));
 
-            //bundles.Add(new StyleBundle("~/Content/css").Include(
-            //        "~/Content/Site.css",
-            //        "~/Content/bootstrap-responsive.min.css"));
+            //bundles.Add(new StyleBundle("~/Content/css").Include("~/css/Site.css"));
 
-            //bundles.Add(new StyleBundle("~/Content/select2").Include(
-            //            "~/Content/select2-bootstrap/select2.css",
-            //            "~/Content/select2-bootstrap/select2-bootstrap.css"));
+            bundles.Add(new StyleBundle("~/Content/select2").Include(
+                        "~/css/select2-bootstrap/select2.css",
+                        "~/css/select2-bootstrap/select2-bootstrap.css"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.min.js"));
-            //bundles.Add(new ScriptBundle("~/bundles/app").Include("~/Scripts/app.js"));
-            //bundles.Add(new StyleBundle("~/Content/bootstrap/base").Include("~/Content/bootstrap.min.css", "~/Content/base/jquery-ui.css", "~/Content/base/jquery.ui.theme.css"));
-            //bundles.Add(new StyleBundle("~/Content/bootstrap/theme").Include("~/Content/bootstrap-theme.min.css"));
-            //bundles.Add(new StyleBundle("~/Content/font-awesome").Include("~/Content/font-awesome.min.css"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/js/bootstrap.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/app")
+                .Include("~/js/app.js")
+                .Include("~/js/bootstraptable/bootstrap-table.min.js")
+                .Include("~/js/bootstraptable/bootstrap-table-th-TH.min.js")
+                .Include("~/js/autoNumeric.min.js")
+                );
+            bundles.Add(new StyleBundle("~/Content/bootstrap/base").Include("~/css/bootstrap.min.css", "~/css/base/jquery-ui.css", "~/css/themes/base/jquery.ui.theme.css"));
+            bundles.Add(new StyleBundle("~/Content/bootstrap/theme").Include("~/css/bootstrap-theme.min.css"));
+            bundles.Add(new StyleBundle("~/Content/font-awesome").Include("~/css/font-awesome.min.css"));
         }
 
         public static void AddDefaultIgnorePatterns(IgnoreList ignoreList)
